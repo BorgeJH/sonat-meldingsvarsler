0a724b85435206c1970e4d57b8a59ce7225b7471
Børge Haugsdal <borge.haugsdal@sonat.no>
2020-02-27 23:59:01 +0100

    Andre versjon

--- meldingsvarsler-oppgave/src/main/java/no/sonat/meldingsvarsler/MeldingSender.java
+++ meldingsvarsler-oppgave/src/main/java/no/sonat/meldingsvarsler/MeldingSender.java
@@ -1,30 +1,94 @@
 package no.sonat.meldingsvarsler;
 
-import no.sonat.meldingsvarsler.meldinger.sms.SMSAbonnent;
-import no.sonat.meldingsvarsler.intrastructure.AbonnentFileRepository;
-import no.sonat.meldingsvarsler.meldinger.sms.SMSMelding;
-import no.sonat.meldingsvarsler.meldinger.sms.SMSProsessor;
-
 import java.io.*;
-import java.util.List;
+import java.util.*;
 
 public class MeldingSender {
-    private List<SMSAbonnent> abonnenter;
+    public Map<String, String> smsAbonnenter = new HashMap<>();
+    public Map<String, String> epostAbonnenter = new HashMap<>();
 
-    public void hentAbonnenter() {
-        abonnenter = new AbonnentFileRepository().hentAbonnenter();
+    public void sendSmsMelding(String melding) {
+        smsAbonnenter.entrySet().
+            forEach(abonnent -> sendSms(abonnent.getKey() , abonnent.getValue(), melding));
     }
 
-    public void leggTilAbonnent(SMSAbonnent smsAbonnent) throws IOException {
-        new AbonnentFileRepository().leggTilAbonnent(smsAbonnent);
+    public void sendEpostMelding(String epostTittel, String epostTekst) {
+        epostAbonnenter.entrySet()
+                .forEach(abonnent -> sendEpost(abonnent.getKey(), abonnent.getValue(), epostTittel, epostTekst));
     }
 
-    public void sendMelding(SMSMelding melding) {
-        abonnenter.forEach(abonnent -> sendSms(abonnent, melding));
+    public void hentEpostAbonnenter() {
+        epostAbonnenter.clear();
+        File file = new File("epostabonnenter.csv");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            StringBuffer buffer = new StringBuffer();
+            while((line = reader.readLine()) != null) {
+                List<String> values = Arrays.asList(line.toString().split(","));
+                epostAbonnenter.put(values.get(0),values.get(1));
+
+            }
+        } catch (IOException e) {
+        }
+
     }
 
-    private void sendSms(SMSAbonnent abonnent, SMSMelding melding) {
-        new SMSProsessor().sendSms(abonnent, melding);
+    public void hentSmsAbonnenter() {
+        smsAbonnenter.clear();
+        File file = new File("smsabonnenter.csv");
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+            StringBuffer buffer = new StringBuffer();
+            while((line = reader.readLine()) != null) {
+                List<String> values = Arrays.asList(line.toString().split(","));
+                smsAbonnenter.put(values.get(0),values.get(1));
+
+            }
+        } catch (IOException e) {
+        }
     }
 
+    public void leggTilSmsAbonnent(String navn, String telefonnummer) {
+        smsAbonnenter.put(navn, telefonnummer);
+    }
+
+    public void leggTilEpostAbonnent(String epost, String navn) {
+        epostAbonnenter.put(epost, navn);
+    }
+
+    public void lagreSmsAbonnenter() {
+        File file = new File("smsabonnenter.csv");
+        FileWriter fileWriter = null;
+        try {
+            StringBuffer sb = new StringBuffer();
+            smsAbonnenter.entrySet()
+                    .forEach(abonnent -> sb.append(abonnent.getKey() +"," +  abonnent.getValue()));
+
+            fileWriter = new FileWriter(file);
+            fileWriter.append(sb.toString());
+        } catch (IOException e) {
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendSms(String navn, String telefonnummer, String tekstmelding) {
+        System.out.println("Sender SMS til "
+                + telefonnummer
+                + "(" + navn + ")"
+                + " => " + tekstmelding
+        );
+    }
+
+    private void sendEpost(String epostAdresse, String navn, String eposttittel, String epostmelding) {
+        System.out.println("Sender Epost til "
+                + epostAdresse
+                + "(" + navn + ")"
+                + " => " + eposttittel
+                + " => " + epostmelding);
+    }
 }
--- meldingsvarsler-oppgave/src/test/java/no/sonat/meldingsvarsler/SMSVarslerTest.java
+++ meldingsvarsler-oppgave/src/test/java/no/sonat/meldingsvarsler/SMSVarslerTest.java
@@ -1,21 +1,34 @@
 package no.sonat.meldingsvarsler;
 
-import no.sonat.meldingsvarsler.meldinger.sms.SMSMelding;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 
 class SMSVarslerTest {
 
     @Test
-    public void sendMeldingerTest() throws IOException {
+    public void sendSmsMeldingerTest() throws IOException {
         MeldingSender smsVarsler = new MeldingSender();
-        smsVarsler.hentAbonnenter();
-        SMSMelding smsMelding = new SMSMelding("Vårens kleskolleksjon har kommet. Løp og kjøp. Førstemann til mølla!!");
-        smsVarsler.sendMelding(smsMelding);
+        smsVarsler.hentSmsAbonnenter();
+        String smsMelding = "Vårens kleskolleksjon har kommet. Løp og kjøp. Førstemann til mølla!!";
+        smsVarsler.sendSmsMelding(smsMelding);
+    }
+
+    @Test
+    public void sendEpostMeldingerTest() throws IOException {
+        MeldingSender smsVarsler = new MeldingSender();
+        smsVarsler.hentEpostAbonnenter();
+        String epostTittel = "Tilbud";
+        String epostMelding = "Vårens kleskolleksjon har kommet. Løp og kjøp. Førstemann til mølla!!";
+        smsVarsler.sendEpostMelding(epostTittel, epostMelding);
     }
 
     @Test
     public void statistikk() {
-        // Skriv ut antall abonnenter per melding
+        MeldingSender smsVarsler = new MeldingSender();
+        smsVarsler.hentSmsAbonnenter();
+        smsVarsler.hentEpostAbonnenter();
+
+        System.out.println("SMS Abonnenter : " + smsVarsler.smsAbonnenter.entrySet().size());
+        System.out.println("Epost Abonnenter : " + smsVarsler.epostAbonnenter.entrySet().size());
     }
 }
